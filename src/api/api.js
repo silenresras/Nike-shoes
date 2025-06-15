4a846a7a007a0762469138c2a69d70876604a4e6
RESRAS SILEN <resrassilen@RESRASs-MacBook-Pro.local>
2025-06-15 04:41:23 -0700

    adjusted base URLS

--- src/api/api.js
+++ src/api/api.js
@@ -2,12 +2,9 @@
 import axios from "axios";
 
 // Base URLs
-/*const BASE_API_URL = import.meta.env.MODE === "development"
+const BASE_API_URL = import.meta.env.MODE === "development"
   ? "http://localhost:7000/api"
-  : "https://auth-backend-api-ce94.onrender.com";*/
-
-const BASE_API_URL = "https://auth-backend-api-ce94.onrender.com/api";
-
+  : "https://auth-backend-api-ce94.onrender.com";
 
 // Individual endpoint groups
 const AUTH_URL = `${BASE_API_URL}/auth`;
